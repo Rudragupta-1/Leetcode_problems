14a7b02e070281ea9f73554257a5ae9c198314f9
Rudra Gupta <114164855+Rudragupta-1@users.noreply.github.com>
2024-03-16 01:54:47 +0530

    Create palindromic_substrings.java

---
+++ Recursion/palindromic_substrings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int even=0;
+        int odd=0;
+        for(int axis=0;axis<s.length();axis++)
+        {
+            for(int orbit=0;axis-orbit>=0&& axis+orbit<s.length();orbit++)
+            {
+                if(s.charAt(axis-orbit)!=s.charAt(axis+orbit))
+                break;
+                odd++;
+            }
+            
+        }
+        for(double axis=0.5;axis<s.length();axis++)
+        {
+            for(double orbit=0.5;axis-orbit>=0&& axis+orbit<s.length();orbit++)
+            {
+                if(s.charAt((int)(axis-orbit))!=s.charAt((int)(axis+orbit)))
+                break;
+                even++;
+            }
+            
+        }
+        return odd+even;
+    }
+}
